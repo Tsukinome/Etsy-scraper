3c540ce9747d2f8ea369b287b5a854d1183587ae
Tsukinome <75446070+Tsukinome@users.noreply.github.com>
2021-03-28 17:09:37 +0300

    Update

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Scraper',
+    version='',
+    packages=['functions'],
+    url='',
+    license='MIT',
+    author='Kristina Grigaitytė',
+    author_email='',
+    description='Scraping Etsy website'
+)
