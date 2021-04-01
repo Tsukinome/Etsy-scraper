477d2b88d72e8377343036dae3477cf21cde1de3
Tsukinome <75446070+Tsukinome@users.noreply.github.com>
2021-04-01 17:43:08 +0300

    Update

--- functions/scraping_functions.py
+++ functions/scraping_functions.py
@@ -9,9 +9,9 @@ from functions import database_functions
 
 def scraper(items: int, keywords: list) -> pd.DataFrame:
     """
-    Scrapes etsy.com website and returns category_id, name, artist
-    price, url_of_image, urls_to_item of each record
-    :param items: number of items to crape of each category. Min - 50.
+    Scrapes etsy.com website and returns keyword_id, title, rating,
+    price, reviews count, item_url, urls_of_images
+    :param items: number of items to crape of each category. Min - 64.
     :param keywords: list of categories to scrape
     :return: DataFrame
     """
@@ -24,7 +24,7 @@ def scraper(items: int, keywords: list) -> pd.DataFrame:
 
         for page in range(1, pages + 1):
 
-            url = f'https://www.etsy.com/search?q={keyword}naruto&page={page}&ref=pagination'
+            url = f'https://www.etsy.com/search?q={keyword}&page={page}'
             page = requests.get(url, headers={'User-Agent': ua.chrome})
             soup = BeautifulSoup(page.content, "html.parser")
             time.sleep(1)
@@ -39,8 +39,8 @@ def scraper(items: int, keywords: list) -> pd.DataFrame:
                 prices.append(price.text.replace(" USD", ""))
             for url_of_image in soup.select("img.img-responsive"):
                 urls_of_image.append(url_of_image["src"])
-            for url_to_item in soup.select("div.description > h3 > a"):
-                urls_to_item.append("https://clone.nl/" + item_url["href"])
+            for item_url in soup.select("div.description > h3 > a"):
+                urls_to_item.append("https://etsy.com/" + item_url["href"])
 
     collected_data = list(zip(keyword_id, title, rating, prices, reviews_count, item_url, urls_of_image))
 
